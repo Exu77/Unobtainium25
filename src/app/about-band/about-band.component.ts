dd347d0039dd1af22657757054746b5a27e7c9d8
Simon Meier <simon.a.meier@gmail.com>
2025-04-21 21:25:25 +0200

    About Band #time 1h #comment Content fixes, styling fixes

--- src/app/about-band/about-band.component.ts
+++ src/app/about-band/about-band.component.ts
@@ -18,7 +18,7 @@ export interface BandMember {
 })
 export class AboutBandComponent {
   public bandMembers: WritableSignal<BandMember[]>;
-
+  public bioWolfgang: WritableSignal<string>;
   constructor() {
     this.bandMembers = signal([
       {
@@ -28,7 +28,7 @@ export class AboutBandComponent {
         imageUrl: 'assets/images/simon.jpg',
 
         bio:
-          'Simon ist der Sänger und Rhytmus-Gitarrist der Band. Er schreibt eigene Songs, in welchen er eigene Erfahrungen verarbeitet. Zusammen mit der Band ' +
+          '<a href="https://www.simonmeier.li">Simon</a> ist der Sänger und Rhytmus-Gitarrist der Band. Er schreibt eigene Songs, in welchen er eigene Erfahrungen verarbeitet. Zusammen mit der Band ' +
           'arbeitet er an den Arrangements und er Produktion der Songs. Beruflich programmiert er Software. Er ist verheiratet und Vater eines Sohnes.  ',
       },
       {
@@ -38,8 +38,8 @@ export class AboutBandComponent {
         imageUrl: 'assets/images/gloor.jpg',
         imageCopyRight: 'J.Konrad Photography',
         bio:
-          'Christian ist der Lead-Gitarrist der Band. Er bereichert die Band mit seinen kreativen Ideen auf der Gitarre, seinen Solis und seinem Background-Gesangt. ' +
-          ' Solo ist er als NotACockRoach auf Youtube zu sehen. Auch er ist professioineller Software-Entwickler. Falls er privat nicht am Musizieren oder Sport machen ist, reist er gerne nach Amerika, um mit seiner Freundin dort die Zeit zu geniessen.',
+          'Christian ist der Lead-Gitarrist der Band. Er bereichert die Band mit seinen kreativen Ideen auf der Gitarre, seinen Solis und seinem Background-Gesang. ' +
+          ' Solo ist er als <a href="https://www.youtube.com/@notacockroach1633">NotACockRoach</a> auf Youtube zu sehen. Auch er ist professioineller Software-Entwickler. Falls er privat nicht am Musizieren oder Sport machen ist, reist er gerne nach Amerika, um mit seiner Freundin dort die Zeit zu geniessen.',
       },
       {
         id: 3,
@@ -56,9 +56,14 @@ export class AboutBandComponent {
         role: 'Bassist',
         imageUrl: '',
         bio:
-          'Dieter der Bassist, spielte zuerst Handorgel in der Band. Er wechselte dann auf eine elektrische Handorgel mit Synthesizer Sounds und begann dann Bass zu lernen. Auch er ist beruflich ' +
+          'Dieter der Bassist, spielte zuerst Akkordeon in der Band. Er wechselte dann auf ein elektrisches Akkorden mit Synthesizer Sounds und begann dann Bass zu lernen. Auch er ist beruflich ' +
           'Software-Entwickler, verheiratet und Vater von drei Kindern.',
       },
     ]);
+    this.bioWolfgang = signal(
+      '<a href="http://www.wolfgangvetsch.li">Wolfgang Vetsch</a> ist unser Band-Coach. Er ist ein bekannter liechtensteinischer Musiker, Komponist, Gitarren-Lehrer und Ensemblen-Coach ' +
+        'der <a href="https://www.musikschule.li/team/wolfgang-vetsch">liechtensteinischen Musikschule</a>. Er spielt in diversen Bands wie z.B. <a href="https://www.mundart.li">Rääs</a>, <a href="https://www.theroyalfunkforce.com/">Royal Funk Force</a> und seiner eigenen Band <a href="https://www.wolfgangvetsch.li/bluewater-2/">Blue Water</a>. ' +
+        '2022 releaste er sein Album <a href="https://open.spotify.com/intl-de/album/352wUD57Fofin9Uy3EGs2R">"Rewired"</a>. Seit einigen Jahren unterstützt er uns mit seinem musikalischen Können, seinem Wissen und Ideen.',
+    );
   }
 }
--- src/app/about-songs/about-songs.component.ts
+++ src/app/about-songs/about-songs.component.ts
@@ -33,9 +33,9 @@ export class AboutSongsComponent {
           ' "Not sure I understand the purpose of my heart" wird im Lied gesungen und soll zum Ausdruck bringen, dass es noch nicht klar ist,' +
           ' welchen Zweck das eigene Herz verfolgt. Manchmal gehen die Gefühle nach oben, mal nach unten. Manchmal weiss man nicht, wie man sein' +
           ' Gesicht wahren kann, manchmal scheint das Herz verklemmt zu sein. Oft sind wir uns nicht sicher, was wir wollen, manchmal warten wir' +
-          ' und treffen dann endlich eine Entscheidung. Das Lied wurde im Tonstudio <a href="https://www.littlebigbeat.com/"><u>Little Big Beat</u></a> in Eschen aufgenommen und von <a href="https://www.instagram.com/fl.gatsby/"><u>Noah Ospelt</u></a> produziert.' +
-          ' Das Arrangement stammt von <a href="http://wolfgangvetsch.li"><u>Wolfgang Vetsch</u></a> und <a href="https://simonmeier.li/about-me/music/"><u>Simon Meier</u></a>. Markant sind die Pausen der Instrumente im Refrain, eine Idee von Wolfgang.' +
-          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/"><u>Simon Meier</u></a>.',
+          ' und treffen dann endlich eine Entscheidung. Das Lied wurde im Tonstudio <a href="https://www.littlebigbeat.com/">Little Big Beat</a> in Eschen aufgenommen und von <a href="https://www.instagram.com/fl.gatsby/">Noah Ospelt</a> produziert.' +
+          ' Das Arrangement stammt von <a href="http://wolfgangvetsch.li">Wolfgang Vetsch</a> und <a href="https://simonmeier.li/about-me/music/">Simon Meier</a>. Markant sind die Pausen der Instrumente im Refrain, eine Idee von Wolfgang.' +
+          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/">Simon Meier</a>.',
         youtubeLink:
           'https://www.youtube.com/embed/UR-cxI8jZtM?si=KdiDq0IN0SCezocO&amp;start=35',
       },
@@ -51,8 +51,8 @@ export class AboutSongsComponent {
           ' Sehr dankbar bin ich, dass ich meine Frau gefunden habe, die mir alles bedeutet, die ich über alles Liebe und mir unseren tollen Sohn geschenkt hat.' +
           ' Sie hat viel Verständnis für dieses Lied aufgebracht und gar für die Produktion beigetragen. Danke Dir mein Schatz. Vielleicht kann ich mit diesem Lied auch anderen Menschen etwas helfen, die ebenfalls mal eine schwere Zeit durchleben mussten", ' +
           ' schreibt Simon.' +
-          ' Das Lied wurde im <a href="https://www.sae.edu/che/"><u>SAE Institute Zürich</u></a> in Zürich aufgenommen und auch von <a href="https://www.instagram.com/fl.gatsby/"><u>Noah Ospelt</u></a> produziert.' +
-          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/"><u>Simon Meier</u></a>.',
+          ' Das Lied wurde im <a href="https://www.sae.edu/che/">SAE Institute Zürich</a> in Zürich aufgenommen und auch von <a href="https://www.instagram.com/fl.gatsby/">Noah Ospelt</a> produziert.' +
+          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/">Simon Meier</a>.',
         youtubeLink:
           'https://www.youtube.com/embed/Ui3b1SZIBaM?si=Qt1L4CQYv0ihs1qR&amp;start=44',
       },
@@ -65,11 +65,11 @@ export class AboutSongsComponent {
           ' Der Refrain lädt zum Mitsingen ein und bleibt einem im Ohr: „Es isch a Gschenk, 300 Johr gids üs und viel meh söllens no si...“.' +
           ' Es war uns wichtig, ein Lied zu kreieren, das Jung und Alt mitsingen kann und das eine freudige Stimmung verbreitet. Wir wollen sagen, dass wir glücklich sein können, mit dem, was wir mit unserem Land haben.' +
           ' In Mauren wird gestohlen und in Balzers bläst der „Pföh“, heisst es in einer der drei Strophen des Liedes, welche auf die eine oder andere lustige „Gegebenheit“ des Landes eingehen.' +
-          ' Wir haben das Lied 2018 geschrieben und es an dem offiziellen Kontest für den "Liechtenstein Song" der Jubiläumsfeier Liechtensteins von <a href="https://www.liechtenstein-marketing.li/"><u>Liechtenstein Marketing</u></a> eingereicht.' +
-          ' Schlussendlich hat Rahel Oehri-Malin mit ihrem <a href="https://youtu.be/qMEDFDOhP-I?si=6lx6xMalm757XkeC"><u>Do khör i hi</u></a> gewonnen, worauf wir herzlich gratuliert haben. Wir hoffen, dass auch unser Lied den Leuten Spass macht,' +
+          ' Wir haben das Lied 2018 geschrieben und es an dem offiziellen Kontest für den "Liechtenstein Song" der Jubiläumsfeier Liechtensteins von <a href="https://www.liechtenstein-marketing.li/">Liechtenstein Marketing</a> eingereicht.' +
+          ' Schlussendlich hat Rahel Oehri-Malin mit ihrem <a href="https://youtu.be/qMEDFDOhP-I?si=6lx6xMalm757XkeC">Do khör i hi</a> gewonnen, worauf wir herzlich gratuliert haben. Wir hoffen, dass auch unser Lied den Leuten Spass macht,' +
           ' Spass beim Hören und beim Mitsingen zur Melodie.' +
           ' Das Lied wurde im Homestudio von Christian Gloor aufgenommen und auch von ihm abgemischt.' +
-          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/"><u>Simon Meier</u></a>.',
+          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/">Simon Meier</a>.',
         youtubeLink:
           'https://www.youtube.com/embed/dSLL2VR7mJo?si=evlMb_yKCFqo4Brg&amp;start=28',
       },
@@ -79,8 +79,8 @@ export class AboutSongsComponent {
         description:
           'Dies ist eines unserer ältesten Lieder. Es ist ein Liebeslied, in dem davon gesungen wird, dass es einfach schön ist, wenn eine bestimmte Person in der Nähe ist' +
           " und dass man gerne im Herzen der anderen Person wäre: ''I\'ll be right, I'll be right, I'll be right there in your heart''." +
-          ' Das Lied wurde im Studio der <a href="https://www.musikschule.li/"><u>Liechtensteinischen Musikschule</u></a> in Eschen aufgenommen und von <a href="https://stefanfrommelt.li/"><u>Stefan Frommelt</u></a> produziert.' +
-          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/"><u>Simon Meier</u></a>.',
+          ' Das Lied wurde im Studio der <a href="https://www.musikschule.li/">Liechtensteinischen Musikschule</a> in Eschen aufgenommen und von <a href="https://stefanfrommelt.li/">Stefan Frommelt</a> produziert.' +
+          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/">Simon Meier</a>.',
         youtubeLink:
           'https://youtube.com/embed/H7Rthh3LYxE?si=XOG4M4y77AmH79Y4',
       },
@@ -92,9 +92,9 @@ export class AboutSongsComponent {
           ' Der Refrain besingt: "Way, way to go, you should have listened, just listened...  Hey why did you go, didn’t listen, I will listen".' +
           ' Manchmal gibt es Situationen, in denen man nicht richtig zuhört und dann leider die Konsequenzen zu tragen hat. Das Lied' +
           ' ist ein Aufruf. Mit rockigem Sound und eingängigem Refrain, der zum Mitsingen einlädt.' +
-          ' Die Aufnahme stammt vom Event "Bring on the Night 2017", welcher von <a href="https://stefanfrommelt.li/"><u>Stefan Frommelt</u></a> organisiert wurde.' +
+          ' Die Aufnahme stammt vom Event "Bring on the Night 2017", welcher von <a href="https://stefanfrommelt.li/">Stefan Frommelt</a> organisiert wurde.' +
           ' Aufgenommen wurde via Kamera von Christian Gloor.' +
-          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/"><u>Simon Meier</u></a>.',
+          ' Geschrieben von und Copyright: <a href="https://simonmeier.li/about-me/music/">Simon Meier</a>.',
         youtubeLink:
           'https://www.youtube.com/embed/LxImfwtWbmE?si=bDmI3bJ4e9uYw8hZ&amp;start=51',
       },
